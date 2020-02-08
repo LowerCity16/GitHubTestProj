78014641307bc95c0ce8c70f235696555ce440d6
samuil stoynov <zergshary@gmail.com>
2020-02-08 11:37:17 +0200

    Add project files.

---
+++ GitHubTest/GitHubTestDlg.h
@@ -0,0 +1,34 @@
+
+// GitHubTestDlg.h : header file
+//
+
+#pragma once
+
+
+// CGitHubTestDlg dialog
+class CGitHubTestDlg : public CDialogEx
+{
+// Construction
+public:
+	CGitHubTestDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_GITHUBTEST_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
